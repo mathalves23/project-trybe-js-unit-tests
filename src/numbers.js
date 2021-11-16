9967696baef273feea01e919146fceb258b8dd16
Matheus Alves <matheus.aalves@hotmail.com>
2021-11-16 18:24:45 -0300

    Requisito 2

--- src/numbers.js
+++ src/numbers.js
@@ -13,7 +13,7 @@ const numbers = (myArray) => {
   for (let i = 0; i < myArray.length; i += 1) {
     if (typeof myArray[i] !== 'number') {
       return false;
-    }
+    } 
   }
   return true;
 };
